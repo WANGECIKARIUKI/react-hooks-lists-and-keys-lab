df314fa245ffda986f73c98e3ef2dd35621084a8
LUCY <wangecikariu.luc@gmail.com>
2023-08-31 20:58:25 +0300

    Lists

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,18 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const navLinks = links.map((navLinks) => {
+    return (
+      <a href={navLinks} key={navLinks}>{navLinks}</a>
+    )
+   return (
+    <nav>
+      {navLinks}
+    </nav>
+   );
+  }
+   );
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
 }
 
 export default NavBar;
