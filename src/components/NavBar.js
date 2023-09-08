6067efd59095db4d3efcc8827bf4909876edfc0d
LUCY <wangecikariu.luc@gmail.com>
2023-09-08 21:01:34 +0300

    Lists and Keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-  const navLinks = links.map((navLinks) => {
-    return (
-      <a href={navLinks} key={navLinks}>{navLinks}</a>
-    )
+  const navLinks = links.map((navLinks) => ( <a href={"#" + navLinks} key={navLinks}>{navLinks}</a>)); 
    return (
     <nav>
       {navLinks}
     </nav>
    );
   }
-   );
-
-}
 
 export default NavBar;
